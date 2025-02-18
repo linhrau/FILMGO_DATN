52e3907c752a10a23f324c8927688a6aa96c3c99
linhrau <linhntph45998@fpt.edu.vn>
2025-02-18 13:47:18 +0700

    base

--- react-base/src/App.jsx
+++ react-base/src/App.jsx
@@ -1,11 +1,10 @@
-import "./App.scss";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+// import './App.css'
 
 function App() {
-	return (
-		<>
-			<h1></h1>
-		</>
-	);
+  return <></>;
 }
 
 export default App;
