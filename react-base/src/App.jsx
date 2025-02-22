1666302b932e81eb68f290adca411b6c6d269511
linhrau <linhntph45998@fpt.edu.vn>
2025-02-22 20:30:37 +0700

    sidebar

--- react-base/src/App.jsx
+++ react-base/src/App.jsx
@@ -1,10 +1,28 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import { Route, Routes } from "react-router-dom";
+import LayoutAdmin from "./layouts/admin/LayoutAdmin";
+import Dashboard from "./pages/admin/Dashboard";
 // import './App.css'
 
 function App() {
-  return <></>;
+  return <>
+  {/* router admin */}
+    <Routes>
+      <Route path="/admin" element={<LayoutAdmin />}>
+        <Route index element={<Dashboard />} />
+      </Route>
+    </Routes>
+
+ {/* router user */}
+    <Routes>
+      <Route>
+          
+      </Route>
+    </Routes>
+
+  </>;
 }
 
 export default App;
--- react-base/src/main.jsx
+++ react-base/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import CounterContext from "./context/CounterContext.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const queryClient = new QueryClient();
+createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <CounterContext>
+        <App />
+      </CounterContext>
+    </BrowserRouter>
+  </QueryClientProvider>
+);
