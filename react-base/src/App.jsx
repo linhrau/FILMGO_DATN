124925aaedefb66212b31e7193ef9e81d89397fb
nthai3424 <thainguyen3424@gmail.com>
2025-02-24 21:14:16 +0700

    update new code

--- react-base/src/App.jsx
+++ react-base/src/App.jsx
@@ -3,11 +3,16 @@ import { Route, Routes } from "react-router-dom";
 
 import SignUp from "./SignUp";
 import SignIn from "./SignIn";
+import LayoutAdmin from "./layout/LayoutAdmin";
+import Dashbroad from "./Dashbroad";
 
 function App() {
   return (
     <>
       <Routes>
+        <Route path="/admin" element={<LayoutAdmin />}>
+          <Route index element={<Dashbroad />} />
+        </Route>
         <Route path="/signup" element={<SignUp />}></Route>
         <Route path="/signin" element={<SignIn />}></Route>
       </Routes>
---
+++ react-base/src/Dashbroad.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dashbroad = () => {
+  return (
+    <div>
+      <h1>Thống kê</h1>
+    </div>
+  );
+};
+
+export default Dashbroad;
