54b9dd3232421621c5f3283807dc5db10f117da4
DESKTOP-MQR0I53\vietlh <lhv2k4@gmail.com>
2025-02-27 19:04:12 +0700

    update screenService

--- react-base/src/apis/screensService.js
+++ react-base/src/apis/screensService.js
@@ -6,7 +6,7 @@ const getScreensByCinemaId = async (cinema_id) => {
     console.log(res.data);
     return res.data;
   } catch (error) {
-    console.error("Lỗi khi gọi Api cinema", error);
+    console.error("Lỗi khi gọi Api screen", error);
   }
 };
 
