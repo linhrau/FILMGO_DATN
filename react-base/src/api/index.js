ede7502eaf2343869e5711ca12e877365d4c5c47
tungntph45409 <nguyentung10c2@gmail.com>
2025-03-31 22:43:53 +0700

    update

--- react-base/src/api/index.js
+++ react-base/src/api/index.js
@@ -1,16 +1,17 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://filmgo.io.vn/api", // Sửa baseUrl thành baseURL
+  baseURL: "http://filmgo.io.vn/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
   },
 });
+
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("accessToken");
+  const token = localStorage.getItem("access_token");
   if (token) {
-    config.headers["Authorization"] = `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
