8d4ae9ab14ac6cdd3ef6dbd301f09550b49d71ef
DESKTOP-MQR0I53\vietlh <lhv2k4@gmail.com>
2025-04-01 10:15:27 +0700

    login with google in React

--- react-base/src/App.jsx
+++ react-base/src/App.jsx
@@ -3,6 +3,9 @@ import { Routes, Route } from "react-router-dom";
 import LayoutAdmin from "@/layouts/admin/LayoutAdmin";
 import Dashboard from "@/pages/admin/Dashboard";
 import Seat from "@/pages/admin/Seat";
+import SignIn from "./SignIn";
+import GoogleCallback from "@/api/googleCallBack";
+
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
           <Route index element={<Dashboard />} />
           <Route path="seats" element={<Seat />} />
         </Route>
+
+        <Route path="/signin" element={<SignIn />}></Route>
+        <Route path="/auth/google" element={<GoogleCallback />}></Route>
       </Routes>
 
       {/* router user */}
